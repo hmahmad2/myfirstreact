ea2967fc0c1da5b2b9934c253b06cdb7293ed554
Hamaad Ahmad <hamaad.m.ahmad@gmail.com>
2019-11-04 14:18:36 -0600

    studying extracting components

--- src/index.js
+++ src/index.js
@@ -38,14 +38,79 @@ const hello = (val) => 'Hello ' + val;
 //     }
 // }
 
+// Let's stick everything I want to render in an array,
+// because doing them individually in the render() function
+// does not work. At all.
 let renderMe = [];
 renderMe.push(myFirstElement, myModel, <br></br>, hello('World!'));
+
+function Welcome(props) {
+    return <h1>Hello, {props.name}!</h1>
+}
+
+function App() {
+    return (
+        <div>
+            <Welcome name="Steve" />
+            <Welcome name="Angelica" />
+            <Welcome name="Gaylord" />
+        </div>
+    )
+}
+
+function formatDate(date) {
+    return date.toLocaleDateString();
+}
+
+function Avatar(props) {
+  return (
+    <img
+      className="Avatar"
+      src={props.user.avatarUrl}
+      alt={props.user.name}
+    />
+  );
+}
+
+function UserInfo(props) {
+  return (
+    <div className = "UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">{props.user.name}</div>
+    </div>
+  );
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <div className="Comment-text">{props.text}</div>
+      <div className="Comment.date">{formatDate(props.date)}</div>
+    </div>
+  );
+}
+
+const comment = {
+  date: new Date(),
+  text: 'Am I enjoying learning React... ? Sure.',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'https://placekitten.com/g/64/64',
+  },
+};
 
 ReactDOM.render(
     // (myFirstElement,
     // myModel,
     // // hello("world!"),
     // <p>Hello</p>),
-    renderMe,
+    // renderMe,
+    // <App />,
+    <Comment
+      date={comment.date}
+      text={comment.text}
+      author={comment.author}
+    />,
     document.getElementById('root')
 );
